4d9a82e965481bc0cbd3755a7178460ece18a538
Andrea Fernandez <andrea.fernandez@pragma.com.co>
2025-06-04 17:36:51 -0500

    commit-initial

---
+++ domain/model/src/main/java/co/com/pragma/model/ticketing/gateways/TicketingRepository.java
@@ -0,0 +1,4 @@
+package co.com.pragma.model.ticketing.gateways;
+
+public interface TicketingRepository {
+}
---
+++ domain/usecase/src/main/java/co/com/pragma/usecase/ticketing/TicketingUseCase.java
@@ -0,0 +1,6 @@
+package co.com.pragma.usecase.ticketing;
+
+import lombok.RequiredArgsConstructor;
+@RequiredArgsConstructor
+public class TicketingUseCase {
+}
